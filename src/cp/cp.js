c1c6702a84881e85aaf5c7e88b982db5dc943c5e
Artur Ten <artur_ten@epam.com>
2025-10-15 17:54:22 +0500

    feat: finish with cp and wt

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,22 @@
+import { spawn } from "child_process";
+import path from "path";
+import { getDirname } from "../helper.js";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const __dirname = getDirname(import.meta.url);
+  const cp = spawn("node", [
+    path.join(__dirname, "files", "script.js"),
+    ...args,
+  ]);
+
+  cp.stdout.on("data", (data) => {
+    process.stdout.write(data.toString());
+  });
+
+  process.stdin.on("data", (data) => {
+    cp.stdin.write(data);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["--arg1", "--arg2"]);
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,41 @@
+import os from "os";
+import { Worker } from "worker_threads";
+import path from "path";
+import { fileURLToPath } from "url";
+
+export const getDirname = (url) => {
+  const __filename = fileURLToPath(url);
+  return path.dirname(__filename);
+};
+
+export const FS_ERROR_MESSAGE = "FS operation failed";
+
 const performCalculations = async () => {
-    // Write your code here
+  const __dirname = getDirname(import.meta.url);
+  const NUM = 10;
+
+  const workers = os.cpus().map(
+    (_, i) =>
+      new Promise((resolve) => {
+        const worker = new Worker(path.join(__dirname, "worker.js"), {
+          workerData: { num: NUM + i },
+        });
+        const result = { status: "error", data: null };
+
+        worker.on("message", (data) => {
+          result.status = "resolved";
+          result.data = data;
+        });
+
+        worker.on("exit", () => resolve(result));
+      })
+  );
+
+  const results = await Promise.all(workers);
+
+  console.log(results);
+
+  return results;
 };
 
 await performCalculations();
